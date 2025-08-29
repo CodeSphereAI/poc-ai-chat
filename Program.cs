0ba86d6ab67750dcefae35b9c2f8a9e4ba17bbd1
Maksym Kostyshyn <maksym.kostyshyn.pz.2024@lpnu.ua>
2025-08-29 11:28:16 +0300

    Create simple chat with AI agent

--- Program.cs
+++ Program.cs
@@ -1,9 +1,36 @@
-﻿namespace ChatWithAI;
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+
+namespace ChatWithAI;
 
 public class Program
 {
-    private static void Main(string[] args)
+    private static readonly string AiModel = "gpt-5-nano";
+
+    private async static Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+        string openAiApiKey = configuration["OpenAI:ApiKey"];
+        ChatClient client = new(AiModel, openAiApiKey);
+
+        Console.WriteLine($"Starting the chat with AI, model: {AiModel}");
+
+        ChatCompletion chat = await client.CompleteChatAsync("Hello");
+        Console.WriteLine(chat.Content[0].Text);
+
+        while (true)
+        {
+            Console.Write("Ask anything: ");
+            string? request = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(request))
+            {
+                break;
+            }
+
+            ChatCompletion response = await client.CompleteChatAsync(request);
+            Console.WriteLine($"AI replied: {response.Content[0].Text}");
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple chat with AI agent
+
+To start using it contact repository owner to receive OpenAI API Key and add it to user secrets, run following commands:
+
+```bash
+dotnet user-secrets init
+dotnet user-secrets set "OpenAI:ApiKey" "your-received-api-key"
+```
