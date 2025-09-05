4320c6a45747a22b958c80bb68da8dec3e339d26
Maksym Kostyshyn <maksym.kostyshyn.pz.2024@lpnu.ua>
2025-09-05 10:23:41 +0300

    Turn console app into API

---
+++ ChatWithAI/Controllers/ChatWithAiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatWithAI.Controllers;
+
+[ApiController]
+[Route("/api/[controller]/")]
+public class ChatWithAiController : ControllerBase
+{
+    [HttpGet]
+    public string GetMessage()
+    {
+        return "Hello, world!";
+    }
+}
--- ChatWithAI/Program.cs
+++ ChatWithAI/Program.cs
@@ -13,38 +13,71 @@ using System.ComponentModel;
 using System.Text;
 using System.Text.Json;
 
-namespace ChatWithAI;
 
-public class Program
+//public class Program
+//{
+//    private static readonly string AiModel = "gpt-5-nano";
+
+//    private async static Task Main(string[] args)
+//    {
+//        var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+//        string openAiApiKey = configuration["OpenAI:ApiKey"] ?? throw new ArgumentException("OpenAi API Key is not present in configuration");
+
+//        AiClientPoc aiClient = new(openAiApiKey, filesStorageDirectoryPath: @"C:\Users\mk099\Desktop\Work\CodeSphereAI\ChatWithAI\Embeddings\files\", embeddingsStorageDirectoryPath: @"C:\Users\mk099\Desktop\Work\CodeSphereAI\ChatWithAI\Embeddings\embeddings\");
+
+//        Console.WriteLine($"Starting the chat with AI, model: {AiModel}");
+
+//        string? userRequest = "Hello!";
+
+//        while (true)
+//        {
+//            var result = await aiClient.AskAsync(userRequest);
+//            ConsoleWriteExtension.WriteColored("AI: ", ConsoleColor.DarkBlue);
+//            Console.WriteLine(result);
+
+//            ConsoleWriteExtension.WriteColored("User: ", ConsoleColor.Green);
+//            userRequest = Console.ReadLine();
+
+//            if (string.IsNullOrWhiteSpace(userRequest))
+//            {
+//                break;
+//            }
+//        }
+//    }  
+//}
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders().AddConsole();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    private static readonly string AiModel = "gpt-5-nano";
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
 
-    private async static Task Main(string[] args)
-    {
-        var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        string openAiApiKey = configuration["OpenAI:ApiKey"] ?? throw new ArgumentException("OpenAi API Key is not present in configuration");
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-        AiClientPoc aiClient = new(openAiApiKey, filesStorageDirectoryPath: @"C:\Users\mk099\Desktop\Work\CodeSphereAI\ChatWithAI\Embeddings\files\", embeddingsStorageDirectoryPath: @"C:\Users\mk099\Desktop\Work\CodeSphereAI\ChatWithAI\Embeddings\embeddings\");
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
-        Console.WriteLine($"Starting the chat with AI, model: {AiModel}");
+var app = builder.Build();
 
-        string? userRequest = "Hello!";
-
-        while (true)
-        {
-            var result = await aiClient.AskAsync(userRequest);
-            ConsoleWriteExtension.WriteColored("AI: ", ConsoleColor.DarkBlue);
-            Console.WriteLine(result);
-
-            ConsoleWriteExtension.WriteColored("User: ", ConsoleColor.Green);
-            userRequest = Console.ReadLine();
-            
-            if (string.IsNullOrWhiteSpace(userRequest))
-            {
-                break;
-            }
-        }
-    }  
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+await app.RunAsync();
